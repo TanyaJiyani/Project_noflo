7fcd6c39e14ebaa607d65498bbffaae6c5d29d97
shravantikatti <34472890+shravantikatti@users.noreply.github.com>
2020-07-28 09:40:19 +0530

    Delete SortValues.py

--- NoFlo_Project/NoFlo_Project/SortValues.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import sys
-import io
-
-data_string = sys.argv[1]
-data = io.StringIO(data_string)
-df = pd.read_csv(data, sep =",") 
-
-print(df.sort_values(by=['ext price']).to_csv(index=False))
